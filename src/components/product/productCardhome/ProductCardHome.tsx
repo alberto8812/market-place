4528a63feb986627edb4b31cbb08a290edb47021
alberto8812 <101751073+alberto8812@users.noreply.github.com>
2024-02-02 08:29:15 -0500

    style card product slide on title

--- src/components/product/productCardhome/ProductCardHome.tsx
+++ src/components/product/productCardhome/ProductCardHome.tsx
@@ -40,25 +40,26 @@ export const ProductCardHome:FC <Props> = ({product}) => {
   return (
         <div className= "h-full ">
           <div className="relative m-0 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md h-[480px]">        
-                <div className="relative mx-3 mt-3 flex h-80 overflow-hidden rounded-xl">
+                <div className="relative mx-3 mt-3  h-80 flex overflow-hidden rounded-xl bg-slate-400 ">
                   <Link href={`/product/${product.slug}`}>  
                       <ProductImage
-                      className="object-cover" 
+                      className="object-cover h-80" 
                       src={imageMouseHover} 
                       alt="product image" 
                       width={400}
                       height={400}
                       setImageMouseHover={setImageMouseHover}
                       imagesfind={imagesfind}
+              
                       
                     />
               
                   </Link>
                     <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">39% OFF</span>
               </div>
-            <div className="mt-4 px-5 pb-5 pt-5">
+            <div className="mt-6 px-5 pb-3 pt-5 h-60 ">
               <Link href={`/product/${product.slug}`}>
-                  <h5 className={`text-xl tracking-tight text-slate-900 ${titleFont.className} hover:underline hover:text-blue-500` }>{title}</h5>
+                  <h5 className={`text-xl tracking-tight text-slate-900 ${titleFont.className} hover:underline hover:text-blue-500` }>{title.length>20?title.slice(0,20)+'...':title}</h5>
               </Link>
               <div className="mt-2 mb-5 flex flex-col items-start justify-between">
                 <div className="flex items-center">

0df17e6f24919d472fa51e8b711ffc05b667d7f9
alberto8812 <101751073+alberto8812@users.noreply.github.com>
2024-03-23 16:58:36 -0500

    install gsap and change branc nav bar

--- src/app/config/fonts.ts
+++ src/app/config/fonts.ts
@@ -1,5 +1,6 @@
-import { Inter,Montserrat_Alternates } from "next/font/google";
+import { Inter,Montserrat_Alternates,Great_Vibes } from "next/font/google";
 
 
 export const inter = Inter({ subsets: ['latin'] })
 export const titleFont = Montserrat_Alternates({ subsets: ['latin'],weight:['500','700'], })
+export const itconFont = Great_Vibes({ subsets: ['latin'],weight:['400'], })
